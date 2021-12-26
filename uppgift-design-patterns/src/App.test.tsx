9b6d00d93574d365f4e2e35744a0a9172b7c3fc4
codeninjah <codeninjahblog@gmail.com>
2021-12-26 19:50:43 +0100

    More basic tests

--- uppgift-design-patterns/src/App.test.tsx
+++ uppgift-design-patterns/src/App.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { MeetupsView } from './components/MeetupsView';
 
 import { shallow, mount } from 'enzyme'
 
@@ -13,5 +14,21 @@ test('renders learn react link', () => {
 describe("Testar med enzyme", () => {
   it("renders the App component", () => {
     render(<App />);
+  })
+})
+
+describe("Test MeetupsView", () => {
+  it("renders MeetupsView", () => {
+    render(<MeetupsView />);
+  })
+  it("renders one child in the view", () => {
+    const wrapper = mount(<MeetupsView />)
+    const content = wrapper.children.length
+    expect(content).toEqual(1)
+  })
+  it("renders the main div within the view", () => {
+    const wrapper = mount(<MeetupsView />)
+    const mainDiv = wrapper.find('div')
+    expect(mainDiv).toBeTruthy()
   })
 })
---
+++ uppgift-design-patterns/src/components/MeetupsView.tsx
@@ -0,0 +1,7 @@
+
+export const MeetupsView = () => {
+    return (
+        <div>
+        </div>
+    )
+}
