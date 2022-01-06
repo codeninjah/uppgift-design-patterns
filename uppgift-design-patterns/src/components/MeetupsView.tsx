7ad3ceb994e032455f53bc23e62fed3fa61dc807
codeninjah <codeninjahblog@gmail.com>
2022-01-06 18:16:43 +0100

    Jobbar på att lösa med sortering utifrån datum

--- uppgift-design-patterns/src/components/MeetupsView.tsx
+++ uppgift-design-patterns/src/components/MeetupsView.tsx
@@ -4,15 +4,24 @@ import { IMeetup } from '../models/Meetup';
 
 export const MeetupsView = () => {
     const meetupsList = Meetups
+    const sortedmeetupsList = Meetups.sort(item => Date.parse(item.date))
     return (
         <div>
             <ul>
                 {
-                    meetupsList.filter(meetup => meetup.name).map(item =>
+                    meetupsList.map(item =>
                         <li key={item.date}> Datum: {item.date} Namn: {item.name}</li>  
                     )
                 }
             </ul>
+
+            <ol>
+                {
+                    sortedmeetupsList.map(item =>
+                        <li key={item.id}>DATE: {item.date}   NAME: {item.name} </li>
+                    )
+                }
+            </ol>
         </div>
     )
 }
