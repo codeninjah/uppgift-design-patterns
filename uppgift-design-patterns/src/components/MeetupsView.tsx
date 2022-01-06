6b6ddc5d4f30a9bb5d897188e244f00d73540b79
codeninjah <codeninjahblog@gmail.com>
2022-01-06 19:47:51 +0100

    Added function where you can filter by manually typing date into text input field

--- uppgift-design-patterns/src/components/MeetupsView.tsx
+++ uppgift-design-patterns/src/components/MeetupsView.tsx
@@ -1,4 +1,5 @@
 import { Key, ReactChild, ReactFragment, ReactPortal } from 'react';
+import React, {useState} from 'react';
 import { Meetups } from '../database.json';
 import { IMeetup } from '../models/Meetup';
 
@@ -7,23 +8,39 @@ export const MeetupsView = () => {
     const meetupsList = Meetups
     const parsedmeetupsList = JSON.parse(JSON.stringify(Meetups))
     const sortedmeetupsList = parsedmeetupsList.sort((a: { date: string; },b: { date: string; }) => (a.date > b.date) ? -1 : ((b.date > a.date) ? 1 : 0))
-    return (
-        <div className="meetups-list">
-            <ul>
-                {
-                    meetupsList.map(item =>
-                        <li key={item.date}> Datum: {item.date} Namn: {item.name}</li>  
-                    )
-                }
-            </ul>
 
-            <ol className="sorted-meetups-list">
-               {
-                   sortedmeetupsList.map((item: any) => 
-                        <li className="sorted-meetups-list-item">DATE: {item.date}   NAME:  {item.name}</li>
-                    )
-               }
-            </ol>
+    const [inputValue, setInputValue] = useState<string>("")
+    return (
+        <div>
+          <div className="inputContainer">
+            <label htmlFor="">Search</label>
+            <input 
+            type="text"
+            data-test="meetups-input"
+            value={inputValue}
+            onChange={(
+              ev: React.ChangeEvent<HTMLInputElement>,
+            ): void => setInputValue(ev.target.value)}
+          />
+        </div>
+            <div className="meetups-list">
+            
+                {/* <ul>
+                    {
+                        meetupsList.map(item =>
+                            <li key={item.date}> Datum: {item.date} Namn: {item.name}</li>  
+                        )
+                    }
+                </ul> */}
+
+                <ol className="sorted-meetups-list">
+                {
+                    sortedmeetupsList.filter((item: any) => item.date.includes(inputValue)).map((item: any) => 
+                            <li className="sorted-meetups-list-item">DATE: {item.date}   NAME:  {item.name}</li>
+                        )
+                }
+                </ol>
+            </div>
         </div>
     )
 }
