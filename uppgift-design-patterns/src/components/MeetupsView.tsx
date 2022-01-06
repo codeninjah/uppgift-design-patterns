8f01ccfb737ce909d33009c244900f0a54415eb8
codeninjah <codeninjahblog@gmail.com>
2022-01-06 17:44:32 +0100

    Making progress

--- uppgift-design-patterns/src/components/MeetupsView.tsx
+++ uppgift-design-patterns/src/components/MeetupsView.tsx
@@ -1,10 +1,20 @@
+import { Meetups } from '../database.json';
+import { IMeetup } from '../models/Meetup';
+
 
 export const MeetupsView = () => {
+    const meetupsList = Meetups
     return (
         <div>
             <ul>
-                <li></li>
+                {
+                    meetupsList.filter(meetup => meetup.name).map(item =>
+                        <li key={item.date}> Datum: {item.date} Namn: {item.name}</li>  
+                    )
+                }
             </ul>
         </div>
     )
 }
+
+export default MeetupsView
