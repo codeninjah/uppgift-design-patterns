f9fb9c6d35cb293bb77548bcfd3a77fe05a93e51
codeninjah <codeninjahblog@gmail.com>
2022-01-06 16:58:48 +0100

    Added database.json file

--- uppgift-design-patterns/src/models/Meetup.tsx
+++ uppgift-design-patterns/src/models/Meetup.tsx
@@ -1,7 +1,20 @@
 
-export interface Meetup {
+export interface IMeetup {
     id: string,
     name: string,
     date: Date,
     comments: [],
+}
+
+/*
+export type Props = {
+    view: (item: IMeetup) => void
+}
+*/
+
+const m1: IMeetup = {
+    id: "12345",
+    name: "ALex",
+    date: new Date(),
+    comments: []
 }
--- uppgift-design-patterns/src/seed.js
+++ uppgift-design-patterns/src/seed.js
@@ -1,11 +1,16 @@
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
+
+//const IMeetup = require('./models/Meetup')
+
 let DateGenerator = require('random-date-generator');
 DateGenerator.getRandomDate(); // random date
 
 let startDate = new Date(2021, 6, 30);
 let endDate = new Date(2022, 1, 20);
 //DateGenerator.getRandomDateInRange(startDate, endDate); // random date in range
+
+//let meetupI = new meetupInterface()
 
 let meetups = [
     "AWS",
@@ -24,14 +29,12 @@ let meetupstestArr = []
 
 for(var meetup of meetups){
     //console.log(meetup)
-    meetupstestArr.push(
-        {
+    meetupstestArr.push({
         "id": uuidv4(),
         "name": meetup,
         "date": DateGenerator.getRandomDateInRange(startDate, endDate),
         "comments": []
-        }
-    )
+        })
 }
 
 for(var meetuptest of meetupstestArr){
