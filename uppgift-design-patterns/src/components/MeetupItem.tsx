b7c52cc17481f9130508eb318324e6d8697560a5
codeninjah <codeninjahblog@gmail.com>
2022-01-12 17:36:37 +0100

    Commenting on events now works

--- uppgift-design-patterns/src/components/MeetupItem.tsx
+++ uppgift-design-patterns/src/components/MeetupItem.tsx
@@ -1,24 +1,34 @@
 import { render } from 'enzyme'
-import { Meetups } from '../database.json'
 import { IMeetup } from '../models/Meetup'
 import { useState } from 'react'
 
+type props = { 
+    id: string,
+    name: string,
+    date: string,
+    comments: string[],
+    addComment: (id:string, comment:string) => void
+ }
 
-const MeetupItem = ({id, name, date, comments}:IMeetup) => {
-    /*
-    let a = localStorage.getItem("comments")
-    let b 
+const MeetupItem = ({id, name, date, comments, addComment}:props) => {
+    let [showButton, setShowButton]  = useState(true);
+    let [showCommentary, setShowCommentary] = useState(false)
+    let [showComments, setShowComments] = useState(comments)
+    let [inputComment, setInputComment] = useState('')
 
-    if(a){
-        b = JSON.parse(a)
-        comments = b.comments
-        console.log("These are the comments " + comments)
+    const attendBtnStuff = () => {
+        setShowButton(false);
+        setShowCommentary(true)
     }
-    else{
-        comments = comments
-    }
-    */
 
+    const Comment = () => {
+        let newComments = showComments
+        showComments.push("Skurt")
+        console.log(newComments)
+        console.log(showComments)
+        setShowComments(showComments)
+    }
+            
         return(
             <>
                 <h1>{name}</h1>
@@ -31,6 +41,10 @@ const MeetupItem = ({id, name, date, comments}:IMeetup) => {
                             comments.map(comments => <li>{comments}</li>) 
                         }
                     </ul> 
+                    { showButton ? <button onClick={attendBtnStuff} data-test="meetups-input">Attend</button> : <></> }
+
+                            { showCommentary ? <div className="member-comment"><button data-test="meetups-comment-btn" onClick={() => addComment(id, inputComment)}>Comment</button>
+                            <input type="text" data-test="meetups-comment-field" placeholder="Your comment here" value={inputComment} onChange={(ev: React.ChangeEvent<HTMLInputElement>,) => setInputComment(ev.target.value)}></input></div> : <></> }
    
                 </div>
             </>
--- uppgift-design-patterns/src/components/MeetupsView.tsx
+++ uppgift-design-patterns/src/components/MeetupsView.tsx
@@ -1,94 +1,25 @@
 import { Key, ReactChild, ReactFragment, ReactPortal } from 'react';
 import React, {useState, useEffect} from 'react';
 import { Meetups } from '../database.json';
-//import { IMeetup } from '../models/Meetup';
 import  MeetupItem  from './MeetupItem';
 import { Props } from '../models/Meetup';
 
-import * as fs from 'fs';
-
-//var fs = require('fs');
-
-
 export const MeetupsView = () => {
-    //const meetupsList = Meetups
     const parsedmeetupsList = JSON.parse(JSON.stringify(Meetups))
-    const sortedmeetupsList = parsedmeetupsList.sort((a: { date: string; },b: { date: string; }) => (a.date > b.date) ? -1 : ((b.date > a.date) ? 1 : 0))
-
-
-    let [showButton, setShowButton]  = useState(true);
-    let [showCommentary, setShowCommentary] = useState(false)
-
-    const meetupsListJSON = JSON.parse(JSON.stringify(Meetups))
-    let [showComments, setShowComments] = useState(meetupsListJSON)
-
-
-    const registerBtnStuff = () => {
-        const register = () => setShowButton(false);
-        const commentary = () => setShowCommentary(true)
-        register();
-        commentary();
-    }
-
-    const ShareComment = (id:any) => {
-        console.log("Testing test")
-        console.log("This is the meetupslist")
-        console.log(showComments) //Skriver ut hela arrayen
-        console.log("Was it one or more?") //YES
-
-        //Ny array och kopiera över den gamla
-        console.log("Är detta nya arrayen?")
-        
-        let newCommentsList = showComments.map((meetup: any) =>{
-            if(meetup.id === id) {
-                meetup.comments.push("CODENINJAH")
-                return meetup
-            }
-            else{
-                return meetup
-            }
+    let sortedmeetupsList = parsedmeetupsList.sort((a: { date: string; },b: { date: string; }) => (a.date > b.date) ? -1 : ((b.date > a.date) ? 1 : 0))
+    
+    let [meetups, setMeetups] = useState(sortedmeetupsList)
+    const [inputValue, setInputValue] = useState<string>("")
+    
+    const addComment = (id: string, comment: string) => {
+        const newMeetupsList = meetups.map((item: any) => {
+            if(item.id === id) item.comments.push(comment) 
+            return item
         })
 
-            setShowComments(newCommentsList)
-
-            //Köra på localStorage?
-            //Lagra sen printa?
-            localStorage.setItem("comments", JSON.stringify(newCommentsList))    
-
-            var a = localStorage.getItem("comments")
-            console.log("a is: " + a) //Prints
-            var b = []
-            if(a) {
-            b = JSON.parse(a) 
-                console.log("b is: " + b[0].name) //Object object object .... 
-                console.log(b[5].date)
-            //var c = newCommentsList.find((item:any) => item.id == id)            
-            //document.getElementsByTagName("li")[0].innerText = newCommentsList
-            }
-
-            /*
-            for(var meetup in newCommentsList){
-                fs.writeFileSync("testfile.json", meetup)
-            }
-            */
-
+        setMeetups(newMeetupsList)
     }
-
-
-    const [inputValue, setInputValue] = useState<string>("")
-    //Följande funktion gör typ inget just nu
-    const goToMeetup = (id: any) => {
-        const meetupinterestedby = sortedmeetupsList.find((item: any) => item.id == id)
-            if(meetupinterestedby){
-                console.log("Alex");
-                console.log(id)
-                console.log(meetupinterestedby)
-                console.log("Was here");
-                meetupinterestedby.comments.push("Alex in the house")
-                return meetupinterestedby
-            }
-    }
-
+    
     //Denna funktion skall jämföra dagens datum med datumen för meetups'en
     //Borde skriva tester innan jag fortsätter bygga ut den
     const dateCompare = (eventDate: any) => {
@@ -122,32 +53,13 @@ export const MeetupsView = () => {
         </div>
             <div className="meetups-list">
 
-                {/* <ol className="sorted-meetups-list">
-                {
-                    sortedmeetupsList.filter((item: any) => item.date.includes(inputValue)).map((item: any) => 
-                            <li className="sorted-meetups-list-item">DATE: {item.date}   NAME:  {item.name}</li>
-                        )
-                }
-                </ol> */}
-
                 <ol className="sorted-meetups-list-test">
                 {
-                    sortedmeetupsList.filter((item: any) => item.date.includes(inputValue)).map((item: any) => 
+                    meetups.filter((item: any) => item.date.includes(inputValue)).map((item: any) => 
                             <li>
-                                <article key={item.id} onClick={() => goToMeetup(item.id)}>
-                                    <MeetupItem key={item.id} id={item.id} name={item.name} date={item.date} comments={item.comments} />
-                                        { showButton ? <button onClick={registerBtnStuff} data-test="meetups-input">Register</button> : <></> }
-                                        {item.commets}
-                                       
-                            {/* Följande har bortkommenterats för att få applikationen att köra igång */}
-                            {/* <ul className="kommentarer">
-                                {
-                                    meetupsListJSON.map((item:any) => <li className="kommentar" key={item}>{item.comments}</li>)                                
-                                }
-                            </ul>  */} 
-
-                                { showCommentary ? <div className="member-comment"><button  data-test="meetups-comment-btn" onClick={() => ShareComment(item.id)}>Share</button>
-                                <input type="text"  data-test="meetups-comment-field" placeholder="Your comment here"></input></div> : <></> }
+                                    { /*article key={item.id} > */ }
+                                 <article key={item.id}> 
+                                    <MeetupItem key={item.id} id={item.id} name={item.name} date={item.date} comments={item.comments} addComment={(id, comment) => addComment(id, comment)} /> 
                                 </article>
                             </li>
                             
